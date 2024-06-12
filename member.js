2ceaee2f1dc0119eb02226488dca0fe90d0d0560
Manish Kumar <158017183+manish-kumar384@users.noreply.github.com>
2024-06-12 11:50:24 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var member = {
+        "id": "member",
+        "name": "member",
+        "sql": "CREATE PROCEDURE member() BEGIN SELECT * FROM member; END"
+    };
+    return member;
+}
